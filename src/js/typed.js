1127442c45f666f6924a7376b384889afdfeb9ed
Maxim Sivokon <maxim@sivokon.me>
2019-04-13 14:33:09 +0300

    Maxim Sivokon v.1.0
    настройки для typed.js
    (без самой либы, либа по ссылке)
    
    если браузерный почтовый клиент — откроет в новом окне
    
    единственный оправданный случай использования target _blank
    
    стилизовал текст и списки
    
    добавил фон
    
    Базовая архитектура; базовые стили текста и блоков
    
    Градиентный заголовок
    
    игнор сгенерированных минифицированных
    файлов
    
    поправил стиль ссылок
    
    gitignore fix
    
    new design and set up
    
    fixed name
    
    main cover design
    
    more acessible from mobile, including safari
    (bottom bar issue)
    
    correct id for script
    
    compact menus
    
    renamed source to avoid auto css creation
    
    stop tracking
    
    stopped tracking
    
    menus screen-side placing
    
    slogan typography
    
    icons renew
    
    added unbreakable space
    
    added logo
    
    better icon
    
    fixed buttons margins on tiniest screens
    
    better hover animation
    
    better nav structure and fixed rotation
    fixed underline issue
    
    favicon and theme settings
    
    new btn hover/focus style
    
    faster animation matching btns
    
    moved logo down in DOM
    for correct auto tabindex
    and logical structure with css off
    
    fix for smallest screens
    
    fix for smallest screens
    
    narrower slogan
    menu alignment
    
    tranisiotn variables
    
    logo icon
    animating
    making more accessible
    
    it's ru version
    
    animating title
    
    breakpoints variables
    
    tinier title
    
    refactoring correct color variables
    
    refactoring
    modulized main navigation block
    
    correct paddings on mobile
    
    better buttons alignment
    
    more adaptive grid
    
    more flexible font-family variables use
    
    typographic experiments
    
    more type experiments
    
    better fitting fallbacks
    
    todo: fix flash of unstyled slogan type
    
    menu orientation on wider screens
    
    more consistent typography
    removed useless variable
    more sensible typeface fallbacks
    
    increas type on desktop
    renamed useless file
    moved some settings from base ( technical)  to global (stylistic)
    
    aligned menu with slogan
    
    .
    
    fix margins with grid off
    
    remove bottom bar fix on desktop
    
    fix logo padding
    
    dynamic typography for more pleasant layouts
    
    logo align with slogan text
    
    logo better aligned with menus
    
    pleasant script settings
    
    temporary hack for desktop

---
+++ src/js/typed.js
@@ -0,0 +1,13 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ["интерфейсы", "веб-сайты", "приложения","пользовательский опыт","цифровые продукты"],
+  typeSpeed: 80,
+  backSpeed: 20,
+  backDelay: 1000,
+  startDelay: 1000,
+  loop: true,
+  smartBackspace: false,
+}
+
+var typed = new Typed("#slogan-typed", options);
