6b311fde51e41383da7de8eac44925ba7ab34463
Maxim Sivokon <maxim@sivokon.me>
2019-04-13 14:38:21 +0300

    настройки для typed.js (без самой либы, либа по ссылке)

---
+++ src/typed.js
@@ -0,0 +1,12 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ["интерфейсы", "веб-сайты", "приложения"],
+  typeSpeed: 40,
+  backSpeed: 0,
+  backDelay: 500,
+  startDelay: 1000,
+  loop: true,
+}
+
+var typed = new Typed("#title-typed", options);
