8a41a8d07c775488e330a99403f0f393d0b0c400
Maxim Sivokon <maxim@sivokon.me>
2019-11-04 01:27:15 +0300

    reconfigured webpack added correct babel/core dep correct path require set up paths

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import Typed from 'typed.js';
+
+var options = {
+  stringsElement: "#typed-strings",
+  typeSpeed: 80,
+  backSpeed: 20,
+  backDelay: 1000,
+  startDelay: 1000,
+  loop: true,
+  smartBackspace: false,
+}
+
+var typed = new Typed("#slogan-typed", options);
