b545dcf2f9f8c445636ba83934ae1226e990a1cb
Maxim Sivokon <maxim@sivokon.me>
2019-11-04 01:27:15 +0300

    reconfigured webpack added correct babel/core dep correct path require set up paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,50 @@
+<<<<<<< HEAD
 const path = require('path');
 
 module.exports = {
 
   entry: './src/js/index.js',
 
+=======
+// webpack.config.js
+var path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/js/index.js',
+>>>>>>> reconfigured webpack
   output: {
     path: path.resolve(__dirname, 'js'),
     filename: 'main.js'
   },
+<<<<<<< HEAD
 
   mode: 'development'
+=======
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              'env'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+>>>>>>> reconfigured webpack
 };
