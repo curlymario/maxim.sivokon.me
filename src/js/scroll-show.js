2fda037bd66b161f96c4bd6ae46fa9fa4ab0d074
Maxim Sivokon <maxim@sivokon.me>
2019-11-21 00:05:04 +0300

    scroll reveal installed

---
+++ src/js/scroll-show.js
@@ -0,0 +1,14 @@
+import ScrollReveal from 'scrollreveal';
+
+ScrollReveal().reveal('.about__heading', { 
+  delay: 200,
+  duration: 1500,
+  origin: 'top',
+  distance: '1.2em'
+});
+ScrollReveal().reveal('.about__paragraph', { 
+  delay: 200,
+  duration: 1000,
+  origin: 'bottom',
+  distance: '1em'
+});
